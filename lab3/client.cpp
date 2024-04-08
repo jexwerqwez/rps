191d967798ab2a69ae48a550cab1e66817249757
Lesley Emile <lesleyem@mi-w6.msk.21-school.ru>
2024-04-08 18:07:07 +0300

    simple config parser

--- lab3/client.cpp
+++ lab3/client.cpp
@@ -1,42 +1,93 @@
-#include <iostream>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <unistd.h>
-#include <string.h>
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
 
 #define PORT 3456
 #define SERVER_ADDRESS "127.0.0.1"
 
-int main() {
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
-    
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error" << std::endl;
-        return -1;
+struct ClientConfig {
+  std::string server_address = "";
+  int port = 0;
+  std::vector<std::string> files = {""};
+};
+
+ClientConfig readConfig(const std::string& filename) {
+  ClientConfig config;
+  std::ifstream file(filename);
+  std::string line;
+  if (!file.is_open()) {
+    std::cerr << "Unable to open file: " << filename << std::endl;
+    return config;
+  }
+  while (getline(file, line)) {
+    std::istringstream iss(line);
+    std::string key;
+    if (getline(iss, key, ':')) {
+      std::string value;
+      getline(iss, value);
+      if (key == "server_address") {
+        config.server_address = value.substr(1);
+      } else if (key == "port") {
+        config.port = std::stoi(value.substr(1));
+      } else if (key == "files") {
+        std::istringstream filestream(value);
+        std::string file;
+        while (filestream >> file) config.files.push_back(file);
+      }
     }
-    
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    
-    if (inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address/ Address not supported" << std::endl;
-        return -1;
-    }
-    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection Failed" << std::endl;
-        return -1;
-    }
-    
-    send(sock, hello, strlen(hello), 0);
-    std::cout << "Hello message sent" << std::endl;
-    valread = read(sock, buffer, 1024);
-    std::cout << buffer << std::endl;
-    
-    close(sock);
-    
-    return 0;
+  }
+  file.close();
+  return config;
+}
+
+int main(int argc, char** argv) {
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+  char* hello = "Hello from client";
+  char buffer[1024] = {0};
+  if (argc < 2) {
+    perror("Usage: ./client <config_file>");
+    exit(EXIT_FAILURE);
+  }
+
+  ClientConfig config = readConfig(argv[1]);
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    std::cerr << "Socket creation error" << std::endl;
+    return -1;
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  if (inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr) <= 0) {
+    std::cerr << "Invalid address/ Address not supported" << std::endl;
+    return -1;
+  }
+
+  if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    std::cerr << "Connection Failed" << std::endl;
+    return -1;
+  }
+
+  send(sock, hello, strlen(hello), 0);
+  std::cout << "Hello message sent" << std::endl;
+  valread = read(sock, buffer, 1024);
+  std::cout << buffer << std::endl;
+  std::cout << "Server Address: " << config.server_address << std::endl;
+  std::cout << "Port: " << config.port << std::endl;
+  std::cout << "Files: ";
+
+  for (const auto& file : config.files) std::cout << file << " ";
+
+  close(sock);
+
+  return 0;
 }
--- lab3/server.cpp
+++ lab3/server.cpp
@@ -1,69 +1,117 @@
-#include <iostream>
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <cstring>
+#include <sys/socket.h>
 #include <unistd.h>
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
 
 #define PORT 3456
 #define HOST INADDR_ANY
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-    
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+struct ServerConfig {
+  std::string server_address = "";
+  int port = 0;
+  std::string directory = "";
+};
+
+ServerConfig readConfig(const std::string &filename) {
+  ServerConfig config;
+  std::ifstream file(filename);
+  std::string line;
+  if (!file.is_open()) {
+    std::cerr << "Unable to open file: " << filename << std::endl;
+    return config;
+  }
+  while (getline(file, line)) {
+    std::istringstream iss(line);
+    std::string key;
+    if (getline(iss, key, ':')) {
+      std::string value;
+      getline(iss, value);
+      if (key == "server_address")
+        config.server_address = value.substr(1);
+      else if (key == "port")
+        config.port = std::stoi(value.substr(1));
+      else if (key == "directory")
+        config.directory = value.substr(1);
     }
-    
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = HOST;
-    address.sin_port = htons(PORT);
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+  }
+  file.close();
+  return config;
+}
+
+int main(int argc, char **argv) {
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  char *hello = "Hello from server";
+
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+    return -1;
+  }
+
+  ServerConfig config = readConfig(argv[1]);
+
+  std::cout << "Server Address: " << config.server_address << std::endl;
+  std::cout << "Port: " << config.port << std::endl;
+  std::cout << "Directory: " << config.directory << std::endl;
+
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = HOST;
+  address.sin_port = htons(PORT);
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    std::cerr << argv[0] << ": bind failed" << std::endl;
+    return -1;
+  }
+
+  if (listen(server_fd, 3) < 0) {
+    std::cerr << argv[0] << ": listen failed" << std::endl;
+    return -1;
+  }
+
+  while (1) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0) {
+      std::cerr << argv[0] << ": accept failed" << std::endl;
+      return -1;
     }
-    
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
+
+    int pid = fork();
+    if (pid < 0) {
+      std::cerr << argv[0] << ": fork failed" << std::endl;
+      return -1;
     }
-    
-    while (1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-        
-        int pid = fork();
-        if (pid < 0) {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        }
-        
-        // Дочерний процесс
-        if (pid == 0) {
-            close(server_fd);
-            
-            // Обработка данных от клиента
-            memset(buffer, 0, 1024);  // Очистка буфера
-            read(new_socket, buffer, 1024);
-            printf("Message from client: %s\n", buffer);
-            send(new_socket, hello, strlen(hello), 0);
-            close(new_socket);
-            
-            exit(0);
-        }
-        // Родительский процесс
-        else {
-            close(new_socket);
-        }
+
+    // Дочерний процесс
+    if (pid == 0) {
+      close(server_fd);
+
+      // Обработка данных от клиента
+      memset(buffer, 0, 1024);  // Очистка буфера
+      read(new_socket, buffer, 1024);
+      printf("Message from client: %s\n", buffer);
+      send(new_socket, hello, strlen(hello), 0);
+      close(new_socket);
+
+      exit(0);
     }
-    
-    return 0;
+    // Родительский процесс
+    else {
+      close(new_socket);
+    }
+  }
+
+  return 0;
 }
